1f389cb0f00272e6b93c069355be24e4e83309dd
David Tejzr <david.tejzr@seznam.cz>
2022-03-10 19:50:48 +0100

    Add project files.

---
+++ TEJ0017_FakturacniSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TEJ0017_FakturacniSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
