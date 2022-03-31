0376e067bc1a369d3c175cbb3dd895f0ecd39eb5
David Tejzr <david.tejzr@atreo.cz>
2022-03-31 14:16:39 +0200

    Corrective tax document - law changes

--- TEJ0017_FakturacniSystem/Models/Document/DocumentTypes/CorrectiveTaxDocument.cs
+++ TEJ0017_FakturacniSystem/Models/Document/DocumentTypes/CorrectiveTaxDocument.cs
@@ -1,7 +1,10 @@
-﻿namespace TEJ0017_FakturacniSystem.Models.Document.DocumentTypes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TEJ0017_FakturacniSystem.Models.Document.DocumentTypes
 {
     public class CorrectiveTaxDocument : Document
     {
+        [Display(Name = "Důvod opravy")]
         public string CorrectionReason { get; set; }
     }
 }
